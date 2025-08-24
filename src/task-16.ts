8a4b7e099e03e68667046c8e6ee3a317ce2fba15
Anastasiia <gorgona1597531597@gmail.com>
2025-08-24 15:42:23 +0200

    update

--- src/task-16.ts
+++ src/task-16.ts
@@ -9,12 +9,18 @@
 // Оголосіть інтерфейс Profile.
 // Додайте до функції явну типізацію, що вона повертає проміс, який приводиться до об'єкта профілю.
 
-// function fetchProfile() {
-//   return new Promise((resolve) => {
-//     setTimeout(() => {
-//       resolve({ username: "max_123", age: 28, isAdmin: true });
-//     }, 1000);
-//   });
-// }
+interface Profile {
+    username: string;
+    age: number;
+    isAdmin: boolean;
+}
 
-// fetchProfile().then(profile => console.log(profile));
+function fetchProfile(): Promise<Profile> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ username: "max_123", age: 28, isAdmin: true });
+    }, 1000);
+  });
+}
+
+fetchProfile().then(profile => console.log(profile));
---
+++ src/task-17.ts
@@ -0,0 +1,35 @@
+// Задача 17
+// Функція fetchUsers повертає проміс, який через fetch отримує список користувачів з API.
+
+// Завдання:
+// Оголосіть інтерфейс User для користувача (перевірте, які властивості користувача містяться у відповіді бекенда).
+// Типізуйте функцію fetchUsers.
+
+interface User {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: {
+        city: string;
+        geo: { lat: string, lng: string };
+        street: string;
+        suite: string;
+        zipcode: string;
+    };
+    company: {
+        bs: string;
+        catchPhrase: string;
+        name: string;
+    }
+    phone: string;
+    website: string;
+}
+
+const fetchUsers = async () : Promise<User> => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  return data;
+};
+
+fetchUsers().then((users) => console.log(users));
