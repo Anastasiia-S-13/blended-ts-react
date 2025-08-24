d425d7198f06a7a3856fd315441a356c438ac617
Anastasiia <gorgona1597531597@gmail.com>
2025-08-24 15:26:52 +0200

    tasks 1-15

--- src/task-1.ts
+++ src/task-1.ts
@@ -1,10 +1,10 @@
 // Задача 1
 // Опис: Є об'єкт settings, який містить налаштування.
-// const settings = {
-//   darkMode: true,
-//   fontSize: 16,
-//   language: "en",
-// };
+const settings = {
+  darkMode: true,
+  fontSize: 16,
+  language: "en",
+};
 
 // Завдання:
 // Створіть тип Settings, який описує цей об'єкт.
